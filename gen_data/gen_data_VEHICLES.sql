22d1b67e44cf7e38146786886b417b8024a43162
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-07-28 22:21:37 +0300

    generate data on t_sa_vehicles

---
+++ gen_data/gen_data_VEHICLES.sql
@@ -0,0 +1,28 @@
+--==============================================================
+-- GENERATING DATA FOR t_sa_vehicles
+--==============================================================
+
+CREATE SEQUENCE vehicle_seq
+    START WITH 1
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE
+    NOMAXVALUE;
+
+
+INSERT INTO sa_vehicles.t_sa_vehicles
+SELECT 
+       vehicle_seq.NEXTVAL                                                          --vehicle_id
+      ,dbms_random.VALUE(1000, 9999)                                                --vehicle_code
+      ,dbms_random.STRING('U', 20)                                                  --vehicle_desc
+      ,dbms_random.STRING('x', 10)                                                  --vehicle_vin
+      ,'mark_' || dbms_random.STRING('U', 5)                                        --vehicle_mark
+      ,TO_DATE(TRUNC(dbms_random.VALUE(2454833,2455197)),'J')                       --insert_dt
+      ,TO_DATE(TRUNC(dbms_random.VALUE(2467833,2955197)),'J')                       --update_dt
+      ,TO_DATE(TRUNC(dbms_random.VALUE(2454833,2555197)),'J')                       --valid_from
+      ,TO_DATE(TRUNC(dbms_random.VALUE(2454833,2755197)),'J')                       --valid_to
+      ,dbms_random.STRING('U', 4)                                                   --fuel_type
+FROM ( 
+SELECT 1
+FROM dual
+CONNECT BY LEVEL <= 300000 );
