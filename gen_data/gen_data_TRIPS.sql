90b515545f9c1f873d2d0e7f7d07a2df75c7f231
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-07-29 23:25:53 +0300

    MORE DATA!!!

--- gen_data/gen_data_TRIPS.sql
+++ gen_data/gen_data_TRIPS.sql
@@ -11,8 +11,9 @@ SELECT
       ,dbms_random.VALUE(1, 300)                                                    --trip_duration
       ,dbms_random.VALUE(1, 200.0)                                                  --trip_cost
       ,dbms_random.STRING('U', 30)                                                  --payment_type
-      ,to_date('2010-01-01', 'yyyy-mm-dd')+dbms_random.value(1,10000)               --insert_dt
-      ,to_date('2010-01-01', 'yyyy-mm-dd')+dbms_random.value(1,10000)               --update_dt
+      ,to_date('2010-01-01', 'yyyy-mm-dd')+dbms_random.value(1,10000)               --trip_date
+      ,dbms_random.VALUE(1, 500000)                                                 --trip_client_id
+      ,dbms_random.VALUE(1, 500000)                                                 --trip_driver_id
 FROM ( 
 SELECT ROWNUM
 FROM dual
