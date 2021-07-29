043ea7ff112fb9751f8d8e2a64e250e3f5bfb882
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-07-29 12:38:22 +0300

    Update gen_data_DRIVERS.sql

--- gen_data/gen_data_DRIVERS.sql
+++ gen_data/gen_data_DRIVERS.sql
@@ -2,21 +2,13 @@
 -- GENERATING DATA FOR t_sa_drivers
 --==============================================================
 
-CREATE SEQUENCE driver_seq
-    START WITH 1
-    INCREMENT BY 1
-    NOCACHE
-    NOCYCLE
-    NOMAXVALUE;
-
-
 INSERT INTO sa_drivers.t_sa_drivers
 SELECT 
-       driver_seq.NEXTVAL                                                           --driver_id
+       ROWNUM                                                                       --driver_id
       ,dbms_random.STRING('U', 20)                                                  --driver_desc
       ,dbms_random.STRING('U', 8)                                                   --license_type
-      ,'driver' || dbms_random.STRING('U', 10)                                      --driver_firstname
-      ,'name__' || dbms_random.STRING('U', 10)                                      --driver_lastname
+      ,'driver' || to_char(ROWNUM)                                                  --driver_firstname
+      ,'name__' || to_char(ROWNUM)                                                  --driver_lastname
       ,TO_DATE(TRUNC(dbms_random.VALUE(2454833,2455197)),'J')                       --hire_dt
       ,TO_DATE(TRUNC(dbms_random.VALUE(2454833,2455197)),'J')                       --insert_dt
       ,TO_DATE(TRUNC(dbms_random.VALUE(2467833,2955197)),'J')                       --update_dt
@@ -25,4 +17,4 @@ SELECT
 FROM ( 
 SELECT 1
 FROM dual
-CONNECT BY LEVEL <= 50000 );
+CONNECT BY LEVEL <= 80000 );
