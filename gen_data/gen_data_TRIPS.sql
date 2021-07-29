c840fb547008959a5a768c1322acfd0093c69589
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-07-29 22:48:38 +0300

    more data!

--- gen_data/gen_data_TRIPS.sql
+++ gen_data/gen_data_TRIPS.sql
@@ -6,14 +6,14 @@ INSERT INTO sa_trips.t_sa_trips
 SELECT 
        ROWNUM                                                                       --trip_id
       ,dbms_random.STRING('U', 20)                                                  --trip_desc
-      ,dbms_random.VALUE(100000, 999999)                                            --trip_from - geo coordinates
-      ,dbms_random.VALUE(100000, 999999)                                            --trip_to - geo coordinates
+      ,dbms_random.VALUE(100000, 555000)                                            --trip_from - geo coordinates
+      ,dbms_random.VALUE(555000, 999999)                                            --trip_to - geo coordinates
       ,dbms_random.VALUE(1, 300)                                                    --trip_duration
-      ,dbms_random.VALUE(1, 200.0)                                                   --trip_cost
+      ,dbms_random.VALUE(1, 200.0)                                                  --trip_cost
       ,dbms_random.STRING('U', 30)                                                  --payment_type
-      ,TO_DATE(TRUNC(dbms_random.VALUE(2454833,2455197)),'J')                       --insert_dt
-      ,TO_DATE(TRUNC(dbms_random.VALUE(2467833,2955197)),'J')                       --update_dt
+      ,to_date('2010-01-01', 'yyyy-mm-dd')+dbms_random.value(1,10000)               --insert_dt
+      ,to_date('2010-01-01', 'yyyy-mm-dd')+dbms_random.value(1,10000)               --update_dt
 FROM ( 
 SELECT ROWNUM
 FROM dual
-CONNECT BY LEVEL <= 150000 );
+CONNECT BY LEVEL <= 1500000 );
